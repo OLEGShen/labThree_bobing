8172e7b5bd1ac1177bf5d47dc71e27d980680f7a
OLEGShen <1592479402@qq.com>
2021-10-07 23:09:00 +0800

    third time

--- assets/scripts/toanswersss.js
+++ assets/scripts/toanswersss.js
@@ -9,28 +9,12 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
     onLoad () {},
-    toanswersss:function(){
-        cc.director.loadScene("playingtwo")
+    to:function(){
+        cc.director.loadScene("playingtwo");
     },
     start () {
 
