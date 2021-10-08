7481cd960c4052f821c63956f6a81b0720ae351c
OLEGShen <1592479402@qq.com>
2021-10-09 00:44:56 +0800

    hope last

--- assets/scripts/cow.js
+++ assets/scripts/cow.js
@@ -8,6 +8,10 @@ const cow_skin = cc.Class({
     }
 });
 
+window.roll = {
+    touzi:null,
+};
+
 cc.Class({
     extends: cc.Component,
 
@@ -21,35 +25,21 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        this.intervalTime = 0;
+
     },
 
     start () {
-        var interval = 0.1;
-        var repeat = 10;
-        var delay = 1;
         var start = 0;
         let cowSet = this.cow_set[0];
         let sprite = this.node.getComponent(cc.Sprite);
-        setInterval(()=>{
+        roll.touzi = setInterval(()=>{
             if(start > 4)
                 start = 0;
             sprite.spriteFrame = cowSet.cows[start]; 
             start++;
          }, 80);
-        // for(var i = 0; i < 10; i++){
-        //     if(start > 4)
-        //         start = 0;
-        //     setTimeout(()=>{sprite.spriteFrame = cowSet.cows[start]; }, 30);
-        //     start++;
-        // }
-        //setTimeout(()=>{console.log("abc"); }, 3000);
-        // Component.schedule(function(){
-        //     if(start > 4)
-        //         start = 0;
-        //     sprite.spriteFrame = cowSet.cows[start];
-        //     start++;
-        // }, interval, repeat, delay);
+
+         
     },
 
 
